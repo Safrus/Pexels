e0d53497bd58a16fb203bc38f2ba8b56bb322683
altron <altron013@users.noreply.github.com>
2020-08-28 18:38:45 +0600

    ui+curated photo integration, fragments created,layouts updated

--- app/src/main/java/kz/tinker/Model.kt
+++
@@ -1,6 +0,0 @@
-package kz.tinker
-
-data class Model(val name:String, val user:Int,  val age: Int) {
-    val imageUrl="https://picsum.photos/150?random=$age"
-    val userUrl="https://picsum.photos/150?random=$user"
-}
---
+++ app/src/main/java/kz/tinker/PhotoListFragment.kt
@@ -0,0 +1,82 @@
+package kz.tinker.pexel
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kz.tinker.MyAdapter
+import kz.tinker.pexel.data.model.Photo
+import kz.tinker.pexel.ui.main.viewmodel.CuratedPhotosViewModel
+import org.koin.android.ext.android.inject
+import org.koin.core.logger.KOIN_TAG
+
+class PhotoListFragment : Fragment(), MyAdapter.PhotoClickListener {
+
+    private val curatedPhotosViewModel: CuratedPhotosViewModel by inject()
+    private var photosList: MutableList<Photo> = mutableListOf()
+
+    private lateinit var adapter: MyAdapter
+    private lateinit var transaction: FragmentTransaction
+    private lateinit var decorator: DividerItemDecoration
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var bundle: Bundle
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.photo_list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindViews(view)
+        setAdapter()
+        getCuratedPhotos()
+    }
+
+    private fun bindViews(view: View) {
+        recyclerView = view.findViewById(R.id.recyclerView)
+        adapter = MyAdapter(this)
+        bundle = Bundle()
+        decorator = DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL)
+        ContextCompat.getDrawable(
+            requireContext(),
+            R.drawable.recyclerline
+        )?.let {
+            decorator.setDrawable(
+                it
+            )
+        }
+    }
+
+    private fun setAdapter() {
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
+        recyclerView.addItemDecoration(decorator)
+    }
+
+    private fun getCuratedPhotos() {
+        curatedPhotosViewModel.getCuratedPhotos()
+        curatedPhotosViewModel.curatedPhotosLiveData.observe(
+            viewLifecycleOwner,
+            Observer { result ->
+                photosList.addAll(result)
+                adapter.setUsers(photosList)
+            })
+    }
+
+    override fun photoClick(position: Int, item: Photo) {
+
+    }
+
+
+}
